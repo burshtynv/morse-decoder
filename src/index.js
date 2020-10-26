205d2bedc2ff06ba0976340b07b76221dc956047
burshtynv <vanyaburshtyn@gmail.com>
2020-10-26 11:31:37 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+  
 const MORSE_TABLE = {
     '.-':     'a',
     '-...':   'b',
@@ -38,7 +39,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let dot = '10';
+    let dash = '11';
+
+    let arr = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        let tmp = expr.slice(i, i + 10);
+        if (tmp.startsWith('*')) {
+            arr.push(' ');
+        } else {
+            let letter = '';
+            for (let j = 0; j < tmp.length; j += 2) {
+                let token = tmp.slice(j, j + 2);
+                if (token === dot) {
+                    letter += '.';
+                } else if (token === dash) {
+                    letter += '-';
+                }
+            }
+            arr.push(letter);
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== ' ') {
+            arr[i] = MORSE_TABLE[arr[i]];
+        }
+    }
+
+    return arr.join('');
 }
 
 module.exports = {
